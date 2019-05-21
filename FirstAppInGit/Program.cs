3528eec4e9a6b7bcd062a48c4f42324d6a09a57d
stian <sai.tian@avepoint.com>
2019-05-21 17:29:20 +0800

    HTML5 test2

--- FirstAppInGit/Program.cs
+++ FirstAppInGit/Program.cs
@@ -8,11 +8,25 @@ namespace FirstAppInGit
 {
     class Program
     {
+
+
+        static void DatetimeTest()
+        {
+            var datetimeNow = DateTime.UtcNow;
+
+            var datetimeStr = new DateTime(datetimeNow.Ticks, DateTimeKind.Utc).ToLocalTime().ToString("dd MMM yyyy (ddd) HH:mm");
+
+            Console.WriteLine("Date Time:{0}.", datetimeStr);
+
+            Console.ReadLine();
+
+        }
         static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
+            DatetimeTest();
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
