6420e0119a90f2eb98e53a058c8e5a68852fde14
stian <sai.tian@avepoint.com>
2019-06-10 11:37:43 +0800

    HTML5 test2

--- FirstAppInGit/Program.cs
+++ FirstAppInGit/Program.cs
@@ -21,12 +21,24 @@ namespace FirstAppInGit
             Console.ReadLine();
 
         }
+
+
+        static void SemesterInfosTest()
+        {
+            List<string> semesterInfos = new List<string>();
+            semesterInfos.Add("AY2018 Term 4");
+            semesterInfos.Add("AY2018 Term 3");
+            semesterInfos.Add("AY2019 Semester 2");
+            Console.WriteLine("Test:{0}.", string.Join("/", semesterInfos));
+            
+        }
         static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
-            DatetimeTest();
+            SemesterInfosTest();
+            //DatetimeTest();
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
